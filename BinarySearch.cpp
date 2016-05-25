eb427dede4a113150c09cf1eb912e5124c41c3d6
sks830 <sksingh830@gmail.com>
2016-05-25 15:18:52 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <chrono>
-
 using namespace std;
+
 // Returns location of key, or -1 if not found
 int BinarySearch(int A[], int l, int r, int key)
 {
@@ -9,40 +8,37 @@ int BinarySearch(int A[], int l, int r, int key)
      while( l <= r )
     {
         m = l + (r-l)/2;
- 
-        if( A[m] == key ) // first comparison
+         if( A[m] == key ) // first comparison
             return m;
- 
-        if( A[m] < key ) // second comparison
+         if( A[m] < key ) // second comparison
             l = m + 1;
         else
             r = m - 1;
     }
- 
-    return -1;
+     return -1;
 }
 
 int main()
 {
      int i=0,x,size,ele;
-     cout<<"Enter the number of element in array";
+     cout<<"Enter the size of the array";
      cin>>size;
-     int* a=new int[size];
+     int* a=new int[size]; //creates dynamic array using new variable.
+     cout<<"Enter the element of array";
      for(i=0;i<size;i++)
-	 {
+      {
          cin>>x;
          a[i]=x;
-              
      }
-     cout<<"Enter the element to search for:";
+     cout<<"Enter the search key:";
      cin>>ele;
-    x=BinarySearch(a,1,size,ele);
-    if(x=-1)
-		cout<<"Element does not exist!!!";
-	else    
-		cout<<"Position of element in an array is: "<<x+1;
-	delete [] a;  // When done, free memory pointed to by a.
-	a = NULL;  	
+     x=BinarySearch(a,1,size,ele); 
+     if(x=-1)
+	cout<<"Element does not exist!!!";
+     else    
+	cout<<"Position of element in an array is: "<<x+1;
+     delete [] a;  // When done, free memory pointed to by a.
+     a = NULL;  	
 }
 
 
